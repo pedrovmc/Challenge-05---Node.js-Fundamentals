0590060a8e74287eb100ad910373810ed56b7dc1
Pedro Carvalho <pedrovmc_@pop-os.localdomain>
2020-11-30 10:33:18 -0300

    first commit

---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,30 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, type, value }: TransactionDTO): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && value > total) {
+      throw Error('outcome value larger than total');
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
